22abfebff92c59ea72a7a940678f1d4f610432bf
FhLk <68800909+FhLk@users.noreply.github.com>
2024-10-26 22:37:42 +0700

    Update CSS @apply Test

--- tao-bin/src/components/Drink/TitleDrink.tsx
+++ tao-bin/src/components/Drink/TitleDrink.tsx
@@ -10,13 +10,13 @@ interface DataTitle {
 const TitleDrink : FC<DataTitle> = ({name,img, category})=> {
     return (
         <div className='flex flex-col items-center'>
-            <Link href={"/menu"} className='w-[100px] text-7xl font-semibold absolute left-10'>&#10094;</Link>
+            <Link href={`/menu/${category}`} className='w-[100px] text-7xl font-semibold absolute left-10'>&#10094;</Link>
             <div className='bg-white w-[200px] rounded-full'>
                 <img src={img} height={200} width={200} />
             </div>
             <div className='text-center'>
                 <h1 className='text-5xl font-semibold py-3'>{name}</h1>
-                <h3 className='text-xl py-3'>{category}</h3>
+                <h3 className='text-3xl py-3 font-semibold'>{category}</h3>
             </div>
         </div>
     )
--- tao-bin/src/components/Drink/TypeDrink.tsx
+++ tao-bin/src/components/Drink/TypeDrink.tsx
@@ -1,16 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function CategoryMenu() {
+  const [isSelected, setIsSelected] = useState<Boolean>(false)
+
+  const selectedOption = (option : number) =>{
+    switch (option) {
+      case 1:
+        
+        break;
+    
+      default:
+        break;
+    }
+  }
   return (
     <div className='w-full my-5'>
         <div className='rounded-xl bg-white h-[180px] px-10 py-5'>
-            <p className='font-semibold text-4xl'>Category Drink</p>
+            <p className='font-semibold text-4xl'>Type Drink</p>
             <div className='flex justify-between h-[100px] items-center my-3'>
                 <img src='./glass-2.svg' height={90} width={90} className='bg-[#FFF2D7] rounded-xl'/>
-                <div className='bg-[#D8AE7E] w-1/2 grid grid-cols-3 justify-items-center rounded-full h-[60px] border-4 border-[#F8C794] font-medium text-3xl '>
-                    <button className='w-full border-r-4 border-[#F8C794] hover:bg-[#C1C1C1] rounded-l-full'>Hot</button>
-                    <button className='w-full border-x-4 border-[#F8C794] hover:bg-[#C1C1C1]'>Cold(+5฿)</button>
-                    <button className='w-full border-l-4 border-[#F8C794] hover:bg-[#C1C1C1] rounded-r-full'>Spin(+10฿)</button>
+                <div className='bg-[#D8AE7E] w-1/2 grid grid-cols-3 gap-0 justify-items-center rounded-full h-[60px] border-4 border-[#F8C794] font-medium text-3xl '>
+                    <button onClick={()=> selectedOption(1)} className={isSelected ? "type-drink-btn-left-selected":`type-drink-btn-left`}>Hot</button>
+                    <button onClick={()=> selectedOption(2)} className='type-drink-btn-middle'>Cold(+5฿)</button>
+                    <button onClick={()=> selectedOption(3)} className='type-drink-btn-right'>Spin(+10฿)</button>
                 </div>
             </div>
         </div>
