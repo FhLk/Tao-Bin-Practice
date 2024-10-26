2c5dba995f93966fb758255c153a08f0bd69ad1b
FhLk <68800909+FhLk@users.noreply.github.com>
2024-10-26 23:34:22 +0700

    Update StrawLib.tsx

--- tao-bin/src/components/Drink/StrawLib.tsx
+++ tao-bin/src/components/Drink/StrawLib.tsx
@@ -8,12 +8,12 @@ function StrawLib() {
             <div className='h-[100px] items-center my-3'>
                 <ul className='space-y-7'>
                     <li className='flex items-center justify-between'>
-                        <img src='./straw.svg' height={90} width={90} className='bg-[#FFF2D7] rounded-xl' />
+                        <img src='/straw.svg' height={90} width={90} className='bg-[#FFF2D7] rounded-xl' />
                         <label htmlFor="horizontal-list-checkbox-straw" className="text-5xl font-medium w-10/12">Get Straw</label>
                         <input id="horizontal-list-checkbox-straw" type="checkbox" value="" name="list-checkbox" className="w-[90px] h-[90px] rounded-xl"/>
                     </li>
                     <li className='flex items-center justify-between'>
-                        <img src='./lid.svg' height={90} width={90} className='bg-[#FFF2D7] rounded-xl' />
+                        <img src='/lid.svg' height={90} width={90} className='bg-[#FFF2D7] rounded-xl' />
                         <label htmlFor="horizontal-list-checkbox-lid" className="text-5xl font-medium w-10/12">Get Lid</label>
                         <input id="horizontal-list-checkbox-lid" type="checkbox" value="" name="list-checkbox" className="w-[90px] h-[90px] rounded-xl"/>
                     </li>
